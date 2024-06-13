43e0d6145a0327394d412104c4e43cce472eb842
Dmytro <vip.taranenko.dima111@gmail.com>
2024-06-13 12:14:52 +0200

    fix

---
+++ src/js/2-gallery.js
@@ -0,0 +1,49 @@
+const STORAGE_KEY = "feedback-form-state";
+const formData = {
+    email: '',
+    message: ''
+};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', e => {
+    e.preventDefault();
+    const formData = new FormData(form)
+    const email = formData.get('email')
+    const message = formData.get('message')
+    const data = { email, message }
+    saveToLS(STORAGE_KEY, data);
+});
+
+window.addEventListener('DOMContentLoaded', e => {
+    const dataLS = loadFromLS(STORAGE_KEY)
+    form.elements.email.value = dataLS?.email || ''
+    form.elements.message.value = dataLS?.message || ''
+})
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const formData = new FormData(form)
+    const email = formData.get('email')
+    const message = formData.get('message')
+    const data = { email, message }
+    form.reset()
+    localStorage.removeItem(STORAGE_KEY)
+});
+
+
+
+function saveToLS(key, value) {
+    const jsonData = JSON.stringify(value);
+    localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+    const jsonValue = localStorage.getItem(key)
+    try {
+        const data = JSON.parse(jsonValue)
+        return data
+    } catch (error) {
+        console.log(error);
+    }
+}
